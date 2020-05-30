af851fe3ed1a26828bd85f9381d046e0ccc9d12c
lanzero <lanheight@outlook.com>
2020-05-29 20:36:32 -0700

    update map

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter as Router,Route} from 'react-router-dom';
 import Home from './Home';
 import News from './News';
 import Refer from './Refer';
+import Maps from './Maps';
 import Contact from './Contact';
 import ShowArticle from './ShowArticle';
 import Pin from './Pin';
@@ -19,8 +20,9 @@ function App() {
       <Route path="/Refer" component={Refer} />
       <Route path="/Contact" component={Contact} />
       <Route path="/Pins/:id" component={Pin} />
-      <Route path="/News/:id" component={Pin} />
+      <Route path="/News/:id" component={ShowArticle} />
       <Route path="/Thanks" component={Thankyou} />
+      <Route path="/Maps" component={Maps} />
     </div>
   </Router>
   );
