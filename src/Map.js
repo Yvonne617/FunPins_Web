090bc581ac10d4f9c7546ae9130fd4839d66e1ee
lanzero <zheanshao@gmail.com>
2020-07-10 20:05:21 -0700

    map component

--- src/Map.js
+++ src/Map.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { Component }  from 'react';
 import "semantic-ui-css/semantic.min.css";
 import './style/Map.css';
 import * as utils from './Maprender';
-utils.initMapbox();
-function Map() {
-  return (
-    <div id="map_container"></div>
-  );
-}
+export default class Map extends Component{
 
-export default Map;
+  render() {
+    utils.initMapbox();
+    return (
+      <div id="map_container"></div>
+    )
+  }
+}
